c0dc0e40cbc680707df4b4053860a1dc725202c4
lastpagevlad <116770372+lastpagevlad@users.noreply.github.com>
2023-08-17 22:42:43 +0300

    update to calculator

--- src/main/java/org/SpringMVCapp/controller/FirstController.java
+++ src/main/java/org/SpringMVCapp/controller/FirstController.java
@@ -1,17 +1,53 @@
 package org.SpringMVCapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+        model.addAttribute("message", "Hello " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                            @RequestParam("action") String action,
+                             Model model){
+        double result;
+        switch (action){
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            case  "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
